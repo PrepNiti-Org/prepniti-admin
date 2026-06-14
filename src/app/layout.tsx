81fa5157e9bb745273090f42f2e50b9537ef3340
Jayant Verma <jayantverma8533@gmail.com>
2026-06-14 19:39:44 +0530

    added them provider code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AdminLayout } from "../components/AdminLayout";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AdminLayout>{children}</AdminLayout>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AdminLayout>{children}</AdminLayout>
+        </ThemeProvider>
       </body>
     </html>
   );
